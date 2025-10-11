eec873a8315384e7bafb8120e8a3f02274644e00
mohamed yasser <119444397+mohamedyasser2609@users.noreply.github.com>
2025-10-11 14:43:09 +0300

    full project
    Co-Authored-By: Mohamed A. Bamakhrama <110086+mohamed@users.noreply.github.com>

---
+++ Debug/FreeRTOS/Source/portable/CCS/ARM_CM3/subdir_vars.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+../FreeRTOS/Source/portable/CCS/ARM_CM3/portasm.asm 
+
+C_SRCS += \
+../FreeRTOS/Source/portable/CCS/ARM_CM3/port.c 
+
+C_DEPS += \
+./FreeRTOS/Source/portable/CCS/ARM_CM3/port.d 
+
+OBJS += \
+./FreeRTOS/Source/portable/CCS/ARM_CM3/port.obj \
+./FreeRTOS/Source/portable/CCS/ARM_CM3/portasm.obj 
+
+ASM_DEPS += \
+./FreeRTOS/Source/portable/CCS/ARM_CM3/portasm.d 
+
+OBJS__QUOTED += \
+"FreeRTOS\Source\portable\CCS\ARM_CM3\port.obj" \
+"FreeRTOS\Source\portable\CCS\ARM_CM3\portasm.obj" 
+
+C_DEPS__QUOTED += \
+"FreeRTOS\Source\portable\CCS\ARM_CM3\port.d" 
+
+ASM_DEPS__QUOTED += \
+"FreeRTOS\Source\portable\CCS\ARM_CM3\portasm.d" 
+
+C_SRCS__QUOTED += \
+"../FreeRTOS/Source/portable/CCS/ARM_CM3/port.c" 
+
+ASM_SRCS__QUOTED += \
+"../FreeRTOS/Source/portable/CCS/ARM_CM3/portasm.asm" 
+
+
---
+++ Debug/FreeRTOS/Source/tasks.d
@@ -0,0 +1,81 @@
+# FIXED
+
+FreeRTOS/Source/tasks.obj: ../FreeRTOS/Source/tasks.c
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/FreeRTOS.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+FreeRTOS/Source/tasks.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOSConfig.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/projdefs.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/portable.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/deprecated_definitions.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/portable/CCS/ARM_CM3/portmacro.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/mpu_wrappers.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/task.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/list.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/timers.h
+FreeRTOS/Source/tasks.obj: C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/stack_macros.h
+
+../FreeRTOS/Source/tasks.c:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOSConfig.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/portable.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/portable/CCS/ARM_CM3/portmacro.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/task.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/list.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/timers.h:
+
+C:/Users/Lenovo/Desktop/graduation\ project/ccs\ project\ workspace/Graduation_Project/FreeRTOS/Source/include/stack_macros.h:
+
